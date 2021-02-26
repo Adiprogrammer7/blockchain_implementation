84f525dbb176d29c9fae7a0fbea683d556718ce7
Aaditya Tile <adi01tile@gmail.com>
2021-02-26 20:20:06 +0530

    understood private and public key concept

--- blockchain_testing.py
+++ blockchain_testing.py
@@ -1,13 +1,18 @@
 import base64
 import ecdsa
 
-# to_string() gives in bytes format, then hex() coverts it in hex format in string.
+# to_string() gives in bytes format, then hex() coverts it in hex code in string format.
 private_key = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
-print(private_key.to_string().hex()) #PRIVATE KEY
+print(private_key.to_string().hex()) #readable private key
 public_key = private_key.get_verifying_key()
 print(public_key.to_string().hex())
 print(base64.b64encode(public_key.to_string())) #encodes bytes format to ascii string format but still in bytes format.
-print(base64.b64encode(public_key.to_string()).decode()) #actual ascii string. # PUBLIC KEY
+print(base64.b64encode(public_key.to_string()).decode()) #actual ascii string. # PUBLIC KEY readable
+
+#using private_key object we can sign on bytes object which gives sign in bytes format and using public key we can verify it.
+msg = b'hello'
+signature = private_key.sign(msg)
+print(public_key.verify(signature, msg)) #tells is the msg signed with private key associated with this public key.
 
 
 # import hashlib
